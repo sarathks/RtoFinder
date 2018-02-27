297e7ea34a7ee9b19829126586c624e18d00889b
sarathsivrud@gmail.com <sarathsivaraman@qburst.com>
2018-02-27 18:13:20 +0530

    removed unsused codes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import HomePage from './containers/HomePage'
 class App extends Component {
   render() {
--- src/containers/home/HomePage.js
+++ src/containers/home/HomePage.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
  
-import Aux from '../hoc/Aux'
-import SearchInput from '../components/Search/SearchInput'
+// import Aux from '../hoc/Aux'
+// import SearchInput from '../components/Search/SearchInput'
 
-class HomePage extends Component {
+// class HomePage extends Component {
 
-  render() {
+//   render() {
     
-    return (
-      <Aux>
+//     return (
+//       <Aux>
 
-        <h1> Vehicle RTO Location </h1>
-        <SearchInput/>
-        <p> Please enter the registration number in space-separated format like "DL 4C AG 3316".</p>
-      </Aux>
-    )
-  }
-}
+//         <h1> Vehicle RTO Location </h1>
+//         <SearchInput/>
+//         <p> Please enter the registration number in space-separated format like "DL 4C AG 3316".</p>
+//       </Aux>
+//     )
+//   }
+// }
 
-export default HomePage
+// export default HomePage
