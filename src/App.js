8446ce5f1e773b3c5bad85ac38b7f94b638a6980
amruthacv <cvamrutha2010@gmail.com>
2018-02-27 17:58:59 +0530

    seting up folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-
+import './App.css';
+import HomePage from './containers/HomePage'
 class App extends Component {
   render() {
     return (
-      <div >
-        
-      </div>
+      <HomePage/>
     );
   }
 }
